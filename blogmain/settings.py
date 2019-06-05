1dd1aad36fac658a997ca76826479918c3311ae1
almost-hero <bgmakar79@gmail.com>
2019-06-05 12:59:00 +0300

    Add README.md and requirements.txt

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -77,17 +77,12 @@ WSGI_APPLICATION = 'blogmain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST':'127.0.0.1',
-        'PORT': 5432
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': 'mydatabase' 
+    } 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
